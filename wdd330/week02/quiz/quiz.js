d4c69db3f1290ff2c35a2d3d548fbd7aa62b5ed0
Eugene Olsen <eugene@olsenfamily.org>
2021-09-23 06:01:46 -0700

    Complete Quiz in NodeJS

--- wdd330/week02/quiz/quiz.js
+++ wdd330/week02/quiz/quiz.js
@@ -1,12 +1,45 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+const fs = require('fs');
 
-  console.clear();
-  
-  readline.question(`What's your name? `, name => {
-    console.log(`Hi ${name}!`)
-    readline.close()
-  });
-  
+process.stdin.setEncoding('utf-8');
+
+const readline = () => {
+    let buffer = new ArrayBuffer(255);
+    let view = new DataView(buffer, 0);
+    let nBytes = fs.readSync(process.stdin.fd, view, 0, 255, 0);
+    return String.fromCharCode.apply(null, new Uint8Array(buffer)).substr(0, nBytes).trimEnd();
+}
+
+const quiz = [
+    ["What is Superman's real name?", "Clark Kent"],
+    ["What is Wonder Woman's real name?", "Diana Prince"],
+    ["What is Batman's real name?", "Bruce Wayne"]
+]
+
+const ask = question => {
+    console.log(question);
+    return readline();
+}
+
+const check = (response, answer) => {
+    if (response === answer) {
+        console.log("Correct!");
+        score++;
+    } else {
+        console.log(`Wrong! The correct answer is ${answer}`);
+    }
+}
+
+console.clear();
+
+console.log("Welcome to the interactive Quiz Ninja!\n");
+
+let score = 0;
+
+// main game loop
+for (const [question, answer] of quiz) {
+    const response = ask(question);
+    check(response, answer);
+}
+
+
+console.log(`\nGame Over.  You scored ${score} point${score !== 1 ? 's' : ''}`);
