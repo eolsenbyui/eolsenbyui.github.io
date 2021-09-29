ba0f4aab5a8ed433186a0267e22f103febdb4a76
Eugene Olsen <eugene@olsenfamily.org>
2021-09-28 19:14:06 -0700

    Add weeks 3 and 4

--- wdd330/js/menu.js
+++ wdd330/js/menu.js
@@ -6,5 +6,13 @@ const links = [
     {
         label: "Week 2 Content and Report",
         url: "./week02/"
+    },
+    {
+        label: "Week 3 Content and Report",
+        url: "./week03/"
+    },
+    {
+        label: "Week 4 Content and Report",
+        url: "./week04/"
     }
 ]
