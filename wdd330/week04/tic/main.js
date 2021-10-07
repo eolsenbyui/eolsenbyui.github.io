3443f89c8a723adcd8b7d762a1475d633751e6fc
Eugene Olsen <eugene@olsenfamily.org>
2021-10-07 04:51:44 -0700

    Refine game

--- wdd330/week04/tic/main.js
+++ wdd330/week04/tic/main.js
@@ -1,8 +1,9 @@
-const player = "O";
-const computer = "X";
+const X = "X";
+const O = "O";
 
 let board_full = false;
 let play_board = Array(9).fill('');  // Initialize play board with empty strings
+let player = X
 
 const board_container = document.querySelector(".play-area");
 
@@ -11,7 +12,7 @@ const winner_statement = document.getElementById("winner");
 check_board_complete = () => {
   let flag = true;
   play_board.forEach(element => {
-    if (element != player && element != computer) {
+    if (element != X && element != O) {
       flag = false;
     }
   });
@@ -22,7 +23,7 @@ const check_line = (a, b, c) => {
   return (
     play_board[a] == play_board[b] &&
     play_board[b] == play_board[c] &&
-    (play_board[a] == player || play_board[a] == computer)
+    (play_board[a] == X || play_board[a] == O)
   );
 };
 
@@ -48,12 +49,12 @@ const check_match = () => {
 
 const check_for_winner = () => {
   let res = check_match()
-  if (res == player) {
-    winner.innerText = "Winner is player!!";
+  if (res == X) {
+    winner.innerText = "Winner is X!";
     winner.classList.add("playerWin");
     board_full = true
-  } else if (res == computer) {
-    winner.innerText = "Winner is computer";
+  } else if (res == O) {
+    winner.innerText = "Winner is O";
     winner.classList.add("computerWin");
     board_full = true
   } else if (board_full) {
@@ -62,13 +63,29 @@ const check_for_winner = () => {
   }
 };
 
+// Build the game board
 const render_board = () => {
-  board_container.innerHTML = ""
+  while (board_container.firstChild) { board_container.removeChild(board_container.firstChild); } // Remove children
+
   play_board.forEach((e, i) => {
-    board_container.innerHTML += `<div id="block_${i}" class="block" onclick="addPlayerMove(${i})">${play_board[i]}</div>`
-    if (e == player || e == computer) {
-      document.querySelector(`#block_${i}`).classList.add("occupied");
-    }
+    let newDiv = board_container.appendChild(document.createElement('div'));
+    newDiv.classList.add("block");
+    newDiv.onclick = addPlayerMove;
+    newDiv.ontouchend = addPlayerMove;
+    newDiv.innerText = play_board[i];
+    newDiv.dataset.index = i;
+
+    if (e == X || e == O) { newDiv.classList.add("occupied"); }
+  });
+
+  // Get rid of outside border
+  play_board.forEach((_, i) => {
+    let children = board_container.children; 
+
+    if ([0, 1, 2].includes(i)) { children[i].classList.add("top-row"); }
+    if ([0, 3, 6].includes(i)) { children[i].classList.add("left-column"); }
+    if ([2, 5, 8].includes(i)) { children[i].classList.add("right-column"); }
+    if ([6, 7, 8].includes(i)) { children[i].classList.add("bottom-row"); }
   });
 };
 
@@ -79,25 +96,29 @@ const game_loop = () => {
 }
 
 const addPlayerMove = e => {
-  if (!board_full && play_board[e] == "") {
-    play_board[e] = player;
+  let index = e.target.dataset.index;
+
+  if (!board_full && play_board[index] == "") {
+    play_board[index] = player;
     game_loop();
-    addComputerMove();
+    player = player == X ? O : X;   // Switch players
+    //addComputerMove();
   }
 };
 
+// Optional, to play with computer
 const addComputerMove = () => {
   if (!board_full) {
     do {
       selected = Math.floor(Math.random() * 9);
     } while (play_board[selected] != "");
-    play_board[selected] = computer;
+    play_board[selected] = O;
     game_loop();
   }
 };
 
 const reset_board = () => {
-  play_board = ["", "", "", "", "", "", "", "", ""];
+  play_board = Array(9).fill('');  // Initialize play board with empty strings
   board_full = false;
   winner.classList.remove("playerWin");
   winner.classList.remove("computerWin");
