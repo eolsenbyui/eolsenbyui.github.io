cb8a5f366e8a1ca6b1c9ef0c08bad1d4d0d45052
Eugene Olsen <eugene@olsenfamily.org>
2021-10-09 12:50:45 -0700

    Comment the code of the Vec class.

--- wdd330/week04/vector/vec.js
+++ wdd330/week04/vector/vec.js
@@ -1,26 +1,34 @@
+// Encapsulates the coordinates for the head of a vector
+// The tail of the vector is implicit at the origin (0, 0)
 class Vec {
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 
+    // The magnitude of the vector
     get length() {
         return Math.sqrt( this.x * this.x + this.y * this.y);
     }
 
+    // The direction of the vector in radians
     get angle() {
         return Math.atan2(this.y, this.x);
     }
 
+    // The direction of the vector in degrees
     get degrees() {
         return this.angle * 180 / Math.PI;
     }
 
     // This use case screams for operator overloading!
+
+    // Add the coordinates of the vector passed in to the coordinates of this vector.
     plus(vec) {
         return new Vec(this.x + vec.x, this.y + vec.y);
     }
 
+    // Subtract the coordinates of the vector passed in from the coordinates of this vector.
     minus(vec) {
         return new Vec(this.x - vec.x, this.y - vec.y);
     }
