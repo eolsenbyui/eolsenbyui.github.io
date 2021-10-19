11eebae66172a0aa3fb4bf47aa4a0e148adc6da3
Eugene Olsen <eugene@olsenfamily.org>
2021-10-18 21:25:39 -0700

    Fill out submit and loaded handlers

--- wdd330/week07/todo/main.js
+++ wdd330/week07/todo/main.js
@@ -1,11 +1,37 @@
 'use strict'
 
+import Todo from "./todo.js";
+
+const todoList = [];
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("tasksLeft").innerText = `${todoList.length} tasks remaining.`
+});
+
 document.getElementById("add-task").addEventListener("submit", (event) => {
     event.preventDefault();
     let element = document.getElementById("new-task");
     let newTask = element.value;
-    element.value = "";
-    console.log("Task Add submitted.")
+    
+    let todo = new Todo(newTask);
+    todoList.push(todo);
+
+    // TODO: Write the todoList array to local storage.
+
+    // Render the To-Do List
+    let table = document.getElementsByTagName("table")[0];
+    table.innerHTML = "";
+
+    let index = 0;
+    for (todo of todoList) {
+        let row = todo.render();
+        row.index = index++;
+        table.appendChild(row);
+    }
+
+    element.value = ""; // Clear the input field
+
+    document.getElementById("tasksLeft").innerText = `${todoList.length} tasks remaining.`
 });
 
 let elements = document.getElementsByClassName("deleteTask");
