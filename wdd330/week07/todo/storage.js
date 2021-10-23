a14e45361fd374afc8309b129b0ec2ee03d172f1
Eugene Olsen <eugene@olsenfamily.org>
2021-10-23 06:09:04 -0700

    Remove functions that can be replaced by one line.

--- wdd330/week07/todo/storage.js
+++ wdd330/week07/todo/storage.js
@@ -1,30 +1,16 @@
-'use strict'
+'use strict';
 
 import Todo from "./todo.js";
 
 export function loadAll() {
     let list = [];
-    let storage = window.localStorage;
-    for (let i = 0; i < storage.length; i++) {
-        let key = storage.key(i);
-        let obj = JSON.parse(storage.getItem(key));
-        list.push(obj);
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let item = localStorage.getItem(key);
+        let obj = JSON.parse(item);
+        let todo = new Todo(obj.content, obj.id, obj.completed);
+        list.push(todo);
     }
 
     return list;
-}
-
-
-export function addItem(todo) {
-    let key = todo.date;
-    let obj = JSON.stringify(todo);
-    storage = window.localStorage;
-    storage.setItem(key, obj);
-}
-
-
-export function removeItem(todo) {
-    let key = todo.date;
-    let storage = window.localStorage;
-    storage.removeItem(key);
 }
