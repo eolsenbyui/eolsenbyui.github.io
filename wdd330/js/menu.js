2a2674807fc6f60a661da1cddd84141c62387660
Eugene Olsen <eugene@olsenfamily.org>
2021-11-02 03:16:34 -0700

    Add Block 2

--- wdd330/js/menu.js
+++ wdd330/js/menu.js
@@ -1,30 +1,39 @@
-const links = [
-    {
-        label: "Week 1 Content and Report",
-        url: "./week01/"
-    },
-    {
-        label: "Week 2 Content and Report",
-        url: "./week02/"
-    },
-    {
-        label: "Week 3 Content and Report",
-        url: "./week03/"
-    },
-    {
-        label: "Week 4 Content and Report",
-        url: "./week04/"
-    },
-    {
-        label: "Week 5 Content and Report",
-        url: "./week05/"
-    },
-    {
-        label: "Week 6 Content and Report",
-        url: "./week06/"
-    },
-    {
-        label: "Week 7 Content and Report",
-        url: "./week07/"
-    }
-]
+const links = {
+    block1: [
+        {
+            label: "Week 1 Content and Report",
+            url: "./week01/"
+        },
+        {
+            label: "Week 2 Content and Report",
+            url: "./week02/"
+        },
+        {
+            label: "Week 3 Content and Report",
+            url: "./week03/"
+        },
+        {
+            label: "Week 4 Content and Report",
+            url: "./week04/"
+        },
+        {
+            label: "Week 5 Content and Report",
+            url: "./week05/"
+        },
+        {
+            label: "Week 6 Content and Report",
+            url: "./week06/"
+        },
+        {
+            label: "Week 7 Content and Report",
+            url: "./week07/"
+        }
+    ],
+
+    block2: [
+        {
+            label: "Week 8 Content and Report",
+            url: "./week08/"
+        }
+    ]
+}
