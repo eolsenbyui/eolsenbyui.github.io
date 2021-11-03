5e76df8c0e010f61da06bea358f2977f64ee3dd1
Eugene Olsen <eugene@olsenfamily.org>
2021-11-03 02:32:27 -0700

    Fix AJAX exercise

--- wdd330/week07/ajax/main.js
+++ wdd330/week07/ajax/main.js
@@ -1,15 +1,13 @@
 'use strict';
 
-const textURL = 'http://numbersapi.com/random';
-const apiURL = 'https://api.chucknorris.io/jokes/random';
+const textURL = 'https://www.boredapi.com/api/activity';
 
-const textButton = document.getElementById('number');
-const apiButton = document.getElementById('chuck');
+const textButton = document.getElementById('bored');
 const outputDiv = document.getElementById('output');
 
 
 textButton.addEventListener('click', async () => {
-    outputDiv.innerHTML = 'Waiting for response...';
+    outputDiv.innerHTML = 'Waiting for response.  The API is slow so please be patient...';
     try {
         let response = await fetch(textURL);
 
@@ -17,26 +15,11 @@ textButton.addEventListener('click', async () => {
             throw new Error(`HTTP error: status = ${response.status}`);
         }
 
-        outputDiv.innerText = await response.text();
+        let json = await response.json();
+
+        outputDiv.innerText = json.activity;
     } catch (error) {
         outputDiv.innerText = 'There was a problem with the fetch() operation: ' + error.message;
     }
 }, false);
 
-
-apiButton.addEventListener('click', () => {
-    outputDiv.innerText = "Not safe for BYU-Idaho or work.";
-    /*
-    fetch(apiURL)
-    .then( response => {
-        outputDiv.innerHTML = 'Waiting for response...';
-    if(response.ok) {
-        return response;
-    }
-    throw Error(response.statusText);
-    })
-    .then( response => response.json() )
-    .then( data => outputDiv.innerText = data.value )
-    .catch( error => console.log('There was an error:', error))
-    */
-},false);
