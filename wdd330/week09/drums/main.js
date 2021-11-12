f73eabd0e5de83e69744d6e3fd454d8ed988a3a3
Eugene Olsen <eugene@olsenfamily.org>
2021-11-12 02:59:05 -0800

    Add transitionend handler, remove ended and keyup

--- wdd330/week09/drums/main.js
+++ wdd330/week09/drums/main.js
@@ -1,10 +1,26 @@
 'use strict';
 
 
+window.addEventListener("load", (event) => {
+    let keys = Array.from(document.querySelectorAll(".key"));
+
+    keys.forEach(key => key.addEventListener("transitionend", onTransitionEnd));
+});
+
+
+function onTransitionEnd(event) {
+    // Many transitions, including transitionend events, occur on different properties.
+    // The property we're interested in is "transform", which is the last property to finish the transition.
+    if (event.propertyName !== "transform") { return; }
+
+    event.target.classList.remove("playing");   // Remove highlighting of key after transition-duration
+}
+
+
 document.addEventListener("keydown", (event) => {
     let code = event.key.charCodeAt();
     if (code > 90) {
-        code -= 32;     // Force to capital letter code.
+        code -= 32;     // Force to uppercase letter code.
     }
 
     let textKey = code.toString();
@@ -14,40 +30,16 @@ document.addEventListener("keydown", (event) => {
 
     let key = document.querySelector(`.key[data-key="${textKey}"]`);
 
-    key.classList.add("playing");   // Highlight the key while its sound plays
+    key.classList.add("playing");   // Highlight the key for transition-duration of key class
 
     // Move the key vertically when pressed, up to 10 times, then reset.
     let top = parseInt(key.style.marginTop, 10) || 0;
-    if (top >= 90) {    // Reset position
-        key.style.marginTop = '0';
+    if (top >= 90) {    
+        key.style.marginTop = '0';  // Reset position
     } else {
         key.style.marginTop = top + 10 + 'px';
     }
 
-
-    // I think the following is too slow.  I prefer the keyup event.
-    audio.addEventListener("ended", (event) => {
-        key.classList.remove("playing");
-    });
-
     audio.currentTime = 0;
     audio.play();
-});
-
-
-//  I much prefer the keyup event for changing the style
-document.addEventListener("keyup", (event) => {
-    return;
-
-    let code = event.key.charCodeAt();
-    if (code > 90) {
-        code -= 32;     // Force to capital letter code.
-    }
-
-    let textKey = code.toString();
-
-    let key = document.querySelector(`.key[data-key="${textKey}"]`);
-    if (!key) { return; }
-    
-    key.classList.remove("playing");
 });
