2042b789c36de01e008e2616a538cc660968306a
Eugene Olsen <eugene@olsenfamily.org>
2021-11-11 18:44:11 -0800

    Optimize selectors and fix stretch goal code.

--- wdd330/week09/drums/main.js
+++ wdd330/week09/drums/main.js
@@ -3,8 +3,7 @@
 const validKeys = [];
 
 window.addEventListener("load", (event) => {
-    let divs = document.querySelectorAll("body > div > div[data-key]");
-    // body > div > div:nth-child(1)
+    let divs = document.querySelectorAll(".key[data-key]");
 
     divs.forEach(div => validKeys.push(div.dataset.key));
 });
@@ -21,16 +20,20 @@ document.addEventListener("keydown", (event) => {
 
     if (!validKeys.includes(textKey)) { return; }
 
-    let audio = document.querySelector(`body > audio[data-key="${textKey}"]`);
+    let audio = document.querySelector(`audio[data-key="${textKey}"]`);
 
-    let key = document.querySelector(`body > div > div[data-key="${textKey}"]`);
+    let key = document.querySelector(`.key[data-key="${textKey}"]`);
 
     key.classList.add("playing");   // Highlight the key while its sound plays
 
     // Move the key vertically when pressed, up to 10 times, then reset.
     let top = parseInt(key.style.marginTop, 10) || 0;
-    if (top > 100) { top = 0; } // Reset position
-    key.style.marginTop = top + 10 + 'px';
+    if (top >= 100) {    // Reset position
+        top = 0; 
+        key.style.marginTop = top + 'px';
+    } else {
+        key.style.marginTop = top + 10 + 'px';
+    }
 
 
     // I think the following is too slow.  I prefer the keyup event.
