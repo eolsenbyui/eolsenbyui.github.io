2f8c711dc81796eec9bcdf4d5d5176580bdcdee4
Eugene Olsen <eugene@olsenfamily.org>
2021-11-25 20:30:42 -0800

    Remove model code, add event handlers.

--- wdd330/week14/final/main.js
+++ wdd330/week14/final/main.js
@@ -1,36 +1,16 @@
 'use strict';
 
-const URL = "https://api.datamuse.com/words?";
+import { getWordList } from "./model.js";
 
 document.getElementById("form").addEventListener("submit", async (event) => {
     event.preventDefault()
 
-    let query = [];
-    let rhymes = document.getElementById("rhyme").value;
-    let near = document.getElementById("near").checked;
-    let starts = document.getElementById("starts").value;
-    let synonym = document.getElementById("synonym").value;
-    if (rhymes) {
-        if (near) {
-            query.push("rel_nry=" + rhymes);
-        } else {
-            query.push("rel_rhy=" + rhymes);
-        }
-    }
+    let wordList = await getWordList();
 
-    if (starts) {
-        query.push("sp=" + starts + "*");
-    }
-
-    if (synonym) {
-        query.push("ml=" + synonym);
-    }
+    if (wordList.length === 0) { return; }
 
     let body = document.getElementById("tbody");
     body.innerHTML = "";
-
-    if (query.length === 0) { return; }
-
 
     let syllables = document.getElementById("syllables").value
     let nSyllables = 0;
@@ -42,47 +22,38 @@ document.getElementById("form").addEventListener("submit", async (event) => {
 
     let nSylValue = nSyl.selectedOptions[0].innerText;
 
-    let url = URL + query.join('&');
 
-    let response = await fetch(url);
+    document.getElementById("credits").classList.add("hidden");
 
-    if (response.ok) {
-        let text = await response.text();
+    // Create table header
+    let thead = document.getElementById("thead");
+    thead.innerHTML = "";
+    for (let key in wordList[0]) {
+        let th = document.createElement("th");
+        if (key === "numSyllables") {
+            th.innerText = "Syllables";
+        } else {
+            th.innerText = key.charAt(0).toUpperCase() + key.slice(1);
+        }
+        thead.appendChild(th);
+    }
 
-        document.getElementById("credits").classList.add("hidden");
-
-        let array = JSON.parse(text);
-
-        // Create table header
-        let header = document.getElementById("header");
-        header.innerHTML = "";
-        for (let key in array[0]) {
-            // TODO: Make first character in key uppercase
-            // TODO: if (key === "numSyllables") => "Syllables";
-            let th = document.createElement("th");
-            th.innerText = key;
-            header.appendChild(th);
+    // Create table rows
+    for (let item of wordList) {
+        if (nSylValue === "exactly") {
+            if (item.numSyllables !== nSyllables) { continue; }
+        } else {
+            if (nSyllables > 0 && item.numSyllables >= nSyllables) { continue; }    // skip
         }
 
-        // Create table rows
-        for (let item of array) {
-            if (nSylValue === "exactly") {
-                if (item.numSyllables !== nSyllables) { continue; }
-            } else {
-                if (nSyllables > 0 && item.numSyllables >= nSyllables) { continue; }    // skip
-            }
+        let row = document.createElement("tr");
+        body.appendChild(row);
 
-            let row = document.createElement("tr");
-            body.appendChild(row);
-
-            for (let value of Object.values(item)) {
-                let cell = document.createElement("td");
-                row.appendChild(cell);
-                cell.innerText = value;
-            }
+        for (let value of Object.values(item)) {
+            let cell = document.createElement("td");
+            row.appendChild(cell);
+            cell.innerText = value;
         }
-    } else {
-        // TODO: do some user-friendly error handling
     }
 });
 
@@ -95,4 +66,19 @@ document.getElementById("nSyl").addEventListener("change", (event) => {
     } else {
         syllables.min = 2;
     }
+});
+
+
+document.getElementById("syllables").addEventListener("input", (event) =>{
+    let trailer = document.getElementById("trailer");
+    if (event.target.value === '1') {
+        trailer.innerText = "syllable";
+    } else {
+        trailer.innerText = "syllables";
+    }
+});
+
+
+document.getElementById("rhyme").addEventListener("input", (event) => {
+    // TODO: If empty, disable the syllables parameters
 });
